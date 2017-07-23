5b0db384db7a078caa9ef6c4240c30099e41c603
Jonathan Widro <jwidro@gmail.com>
2017-07-23 19:47:05 -0400

    2011 10 27 version7 2011 10 27

--- single.php
+++ single.php
@@ -12,7 +12,28 @@
 				by <a href=<?php echo $authorlink ?>><?php echo $insider_display_name ?></a> on <?php the_time('F j, Y'); ?>  | <a href="mailto:<?php the_author_email(); ?>">Email the author</a> <?php edit_post_link('Edit Post','| ',' '); ?>	<?php if($show_twitter){echo $show_twitter_text;	}?>
 			</div>
 			<div class="article_subheadline_right">
-				<img class="subscribe_newsletter" src="/wp-content/themes/version7/images/sharethistemp.png">
+			
+			
+<div style="height:40px;">
+	<div id="fb-root"></div>
+	<div style="float:left;margin:10px;">
+		<fb:like href="<?php the_permalink(); ?>" layout="button_count" show_faces="true" send="false" width="" action="like" font="arial" colorscheme="light"></fb:like> 
+	</div>
+	<div style="float:left;margin:10px;"><div>
+		<a href="http://twitter.com/share" class="twitter-share-button" 
+			data-url="<?php the_permalink(); ?>"
+			data-text="<?php the_title(); ?>"
+			data-count="horizontal">Tweet</a>
+	</div>
+	</div>
+	<div style="float:left;margin:10px;"><script type="text/javascript" src="http://apis.google.com/js/plusone.js">
+                     {lang: 'en-US'}
+              </script><g:plusone size="medium" href="<?php the_permalink(); ?>" count="1"></g:plusone></div><div style="float:right;margin:10px;"><a name="fb_share" type="button_count" share_url="<?php the_permalink(); ?>" href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&amp;t=Confirmed two announcer angles, one on each show">Share</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script></div></div>
+
+</div>
+<br /><br />	        			
+			
+				<!--<img class="subscribe_newsletter" src="/wp-content/themes/version7/images/sharethistemp.png">-->
 			</div>
 
 		</div>
